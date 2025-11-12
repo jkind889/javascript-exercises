1b9d483789ee047c4d8f2dfd5c7864315ecb7acf
jkind889 <jova.kind@bison.howard.edu>
2025-11-12 16:56:00 -0500

    Solution - 06 repeatStrings

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+// so need a for loop def
+let resultstring = ""
+
+
+
+for(let i=0;i<num;i++)
+{
+    resultstring+=string;
+}
+
+if(num < 0)
+{
+    resultstring="ERROR"
+}
+
+return resultstring
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
