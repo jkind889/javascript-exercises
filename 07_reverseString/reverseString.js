c2932d98844c1bee9b3c38e1815f6eea2e7096d0
jkind889 <jova.kind@bison.howard.edu>
2025-11-13 10:14:24 -0500

    Solution 07 -  reverseStrings

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = function(string) 
+{
+
+    let result ="";
+    let decrement= -1;
+    for(let i= 0;i<string.length;i++)
+    {
+        result+=string.at(decrement);
+        decrement--;
+    }
+
+    return result;
+
 
 };
+
+console.log(reverseString("hello world"));
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
