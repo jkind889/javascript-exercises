bb409bfe510caa8fea2cc51d56b2be281d6632ec
jkind889 <jova.kind@bison.howard.edu>
2025-11-13 11:11:48 -0500

    08 solution - removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...theArgs)
+{
+
+for(let i = 0; i<array.length; i++)
+{
+    if(theArgs.includes(array[i]))
+    {
+        let index = i
+        if(index > -1)
+        {
+            array.splice(index,1);
+            i--;
+        }
+    }
+    else
+    {
+      continue;
+    }
+}
+return array;
+
+}; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
