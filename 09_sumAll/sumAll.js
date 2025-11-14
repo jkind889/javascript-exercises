c2a676bc970839dac33d81ed77dccf04a1d820bd
jkind889 <jova.kind@bison.howard.edu>
2025-11-14 10:37:21 -0500

    09 Alt Solution - sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -4,25 +4,33 @@ const sumAll = function(a,b)
 
    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0)
     {
-        return "ERROR"
+        return "ERROR";
     }
 
-    if(b > a)
-    {
-        for(let i = a; i<b+1;i++)
-        {
-            sum+=i;
-        }
-    }
-    else
-    {
-        for(let i = b; i<a+1;i++)
-        {
-            sum+=i;
-        }
-    }
+    // if(b > a)
+    // {
+    //     for(let i = a; i<b+1;i++)
+    //     {
+    //         sum+=i;
+    //     }
+    // }
+    // else
+    // {
+    //     for(let i = b; i<a+1;i++)
+    //     {
+    //         sum+=i;
+    //     }
+    // }
     
-    return sum
+    let start = Math.min(a, b)
+    let end = Math.max(a, b)
+
+    for(i = start; i<=end;i++)
+    {
+        sum+=i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
