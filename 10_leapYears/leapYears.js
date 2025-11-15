05f6a462e9dc1ff96dc6d283eaecb7b3a1f6d50b
jkind889 <jova.kind@bison.howard.edu>
2025-11-15 11:42:18 -0500

    10 Solution - leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year)
+{
+
+    if(year % 4 == 0)
+    {
+        if(!Number.isInteger(year / 100) || Number.isInteger(year / 400))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return false;
 
 };
 
